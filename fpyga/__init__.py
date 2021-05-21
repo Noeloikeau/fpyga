0cfa4c9c8cd564a63eb7998b570c81cb6b0a1a1d
Noeloikeau <noeloikeau@gmail.com>
2021-05-21 11:34:35 -1000

    auto

---
+++ fpyga/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.1"
+
+import sidis
+import mif
+import quartustcl
+import numpy as np
+import os
+from typing import Optional, Tuple, Dict, Callable, Union
+from .devices import *
+from .projects import *
+from .scripting import *
